6ec617f41e4898f9e21b4680d27c3ad13b088159
sp_3141 <07shourya.pratap@gmail.com>
2026-02-14 21:38:53 +0530

    Added conversions.py

---
+++ conversions.py
@@ -0,0 +1,21 @@
+def priority(x):
+    match x:
+        case '^':
+            return 3
+        case '*' | '/':
+            return 2
+        case '+' | '-':
+            return 1
+
+def infix_to_prefix(st: str):
+    pass
+def infix_to_postfix(st: str):
+    pass
+def prefix_to_infix(st: str):
+    pass
+def prefix_to_postfix(st: str):
+    pass
+def postfix_to_infix(st: str):
+    pass
+def postfix_to_prefix(st: str):
+    pass
