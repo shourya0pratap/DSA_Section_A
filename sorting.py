34885338479b87175769b46d8b29ec5dd1307974
sp_3141 <07shourya.pratap@gmail.com>
2026-03-30 23:08:33 +0530

    Added insertionSort

--- sorting.py
+++ sorting.py
@@ -13,3 +13,15 @@ def selectionSort(nums: list[int]):
             if nums[j] < nums[minInd]:
                 minInd = j
         nums[i] , nums[minInd] = nums[minInd] , nums[i]
+        
+def insertionSort(nums: list[int]):
+    n = len(nums)
+    for i in range(1, n):
+        j = i - 1
+        key = nums[i]
+        
+        while j >= 0 and nums[j] > key:
+            nums[j+1] = nums[j]
+            j -= 1
+            
+        nums[j+1] = key
