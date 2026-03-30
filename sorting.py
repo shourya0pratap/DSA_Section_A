ee1da18b85302cc9e45d11f96e59d1f081ebbb55
sp_3141 <07shourya.pratap@gmail.com>
2026-03-30 23:06:57 +0530

    Added selectionSort

--- sorting.py
+++ sorting.py
@@ -4,3 +4,12 @@ def bubbleSort(nums: list[int]):
         for j in range(n-1-i):
             if nums[j] > nums[j+1]:
                 nums[j] , nums[j+1] = nums[j+1] , nums[j]
+
+def selectionSort(nums: list[int]):
+    n = len(nums)
+    for i in range(n-1):
+        minInd = i
+        for j in range(i+1, n):
+            if nums[j] < nums[minInd]:
+                minInd = j
+        nums[i] , nums[minInd] = nums[minInd] , nums[i]
