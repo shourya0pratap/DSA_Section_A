d33606313173df5220343ca4e5cd573b1dffc0b7
sp_3141 <07shourya.pratap@gmail.com>
2026-03-30 23:18:07 +0530

    Added mergeSort

--- sorting.py
+++ sorting.py
@@ -25,3 +25,36 @@ def insertionSort(nums: list[int]):
             j -= 1
             
         nums[j+1] = key
+        
+def mergeSort(nums: list[int]):
+    n = len(nums)
+    if n <= 1:
+        return nums
+    mid = n//2
+    leftHalf = nums[:mid]
+    rightHalf = nums[mid:]
+    sortedLeft = mergeSort(leftHalf)
+    sortedRight = mergeSort(rightHalf)
+    return merge(sortedLeft, sortedRight)
+
+def merge(left: list[int], right: list[int]):
+    m = len(left)
+    n = len(right)
+    result = []
+    i = j = 0
+    while i < m and j < n:
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
+    
+def main():
+    lis = [3,-1,2,4,0]
+    print(mergeSort(lis))
+    
+main()
